92ef27bfe7edc208ccca962f0a7fa05053de8c5c
Emineabayeva <abayevaemine01@gmail.com>
2024-09-22 13:54:38 +0400

    task added

---
+++ s8.js
@@ -0,0 +1,82 @@
+
+telebeler = [
+{
+    ad:"Emine",
+    soyad:"Abayeva",
+    kursIshi: 10,
+    passedJavaScript: true
+},
+{
+    ad:"Ayshen",
+    soyad:"Hesenova",
+    kursIshi: 10,
+    passedJavaScript: true  
+},{
+    ad:"Elnur",
+    soyad:"Resulov",
+    kursIshi: 10,
+    passedJavaScript: true
+},
+{
+    ad:"Elmar",
+    soyad:"Misirzade",
+    kursIshi: 10,
+    passedJavaScript: true
+},
+{
+    ad:"Fidan",
+    soyad:"Sultanli",
+    kursIshi: 10,
+    passedJavaScript: true
+},
+{
+    ad:"Xedice",
+    soyad:"Shirvani",
+    kursIshi: 10,
+    passedJavaScript: true
+},{
+    ad:"Konul",
+    soyad:"Abayeva",
+    kursIshi: 10,
+    passedJavaScript: true
+},
+{
+    ad:"Shehane",
+    soyad:"Zulfaqarova",
+    kursIshi: 10,
+    passedJavaScript: true
+},
+{
+    ad:"Zeyneb",
+    soyad:"Abdulova",
+    kursIshi: 10,
+    passedJavaScript: true
+},{
+    ad:"Aslan",
+    soyad:"Aslanov",
+    kursIshi: 10,
+    passedJavaScript: true
+}
+]
+
+for (let i = 0; i < telebeler.length; i++) {
+console.log(telebeler[i].ad);
+    
+}
+
+// const students = [
+//     { surname: "Aliyev", name: "Elmar", grade: 8, passedJavaScript: true },
+//     { surname: "Məmmədov", name: "Aysel", grade: 6, passedJavaScript: false },
+//     { surname: "Quliyev", name: "Namiq", grade: 9, passedJavaScript: true },
+//     { surname: "Hüseynov", name: "Leyla", grade: 7, passedJavaScript: true },
+//     { surname: "Rəsulov", name: "Ramil", grade: 5, passedJavaScript: false },
+//     { surname: "Həsənova", name: "Gülnar", grade: 10, passedJavaScript: true },
+//     { surname: "Abbasov", name: "Fərid", grade: 4, passedJavaScript: false },
+//     { surname: "Sadiqov", name: "Səbinə", grade: 8, passedJavaScript: true },
+//     { surname: "Qasımov", name: "Rüfət", grade: 7, passedJavaScript: false },
+//     { surname: "Mansurova", name: "Aytən", grade: 9, passedJavaScript: true }
+// ];
+
+// students.forEach(student => {
+//     console.log(`${student.surname} ${student.name} - JavaScript'e buraxılıb: ${student.passedJavaScript}`);
+// });
